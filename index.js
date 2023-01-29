7b397d08579618413dcca2b8417cf287955f4661
muhammad-jiku <muhammadjiku364@gmail.com>
2023-01-29 18:33:53 +0600

    modify all over

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
-const databaseConnect = require('./utils/databaseConnet');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -18,12 +18,114 @@ app.use(cors(corsConfig));
 app.options('*', cors(corsConfig));
 app.use(express.json());
 
-// database connect
-databaseConnect();
-
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'HELLO THERE!!' });
 });
+
+const uri = `mongodb+srv://${process.env.DB_AUTHOR}:${process.env.DB_PASS}@cluster0.vcvbzjp.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+// connecting to the database
+const databaseConnect = async () => {
+  try {
+    await client.connect();
+    const userCollection = client
+      .db(`${process.env.DB_NAME}`)
+      .collection('userInfo');
+
+    app.get('/api/login', async (req, res) => {
+      const { email, password } = await req.body;
+
+      try {
+        const oldUser = await userCollection.find({ email: email }).toArray();
+
+        if (!oldUser) {
+          return res.status(404).json({ message: 'User does not exist' });
+        } else {
+          const isPasswordCorrect = await bcrypt.compare(
+            password,
+            oldUser?.password
+          );
+
+          if (!isPasswordCorrect)
+            return res.status(400).json({ message: 'Something went wrong' });
+
+          const token = jwt.sign(
+            { email: oldUser?.email },
+            process.env.ACCESS_TOKEN_SECRET,
+            {
+              expiresIn: '86400s',
+            }
+          );
+
+          console.log('token...', token);
+          res.status(200).json({
+            message: 'User existence test passed successfully!!',
+            data: oldUser,
+            accessToken: token,
+          });
+        }
+      } catch (err) {
+        // console.log(err);
+        res.status(500).json({
+          message: 'There is a server side error',
+          // error: err
+        });
+      }
+    });
+
+    app.post('/api/registration', async (req, res) => {
+      const { username, phone, email, password } = await req.body;
+
+      try {
+        const oldUser = await userCollection.find({ email: email }).toArray();
+
+        if (!oldUser) {
+          const hashedPassword = await bcrypt.hash(password, 12);
+
+          const newUser = {
+            username: username,
+            phone: phone,
+            email: email,
+            password: hashedPassword,
+          };
+
+          const savedUser = await userCollection.insertOne(newUser);
+
+          const token = jwt.sign(
+            { email: savedUser.email },
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: '86400s' }
+          );
+          console.log('creating token...', token);
+          res.status(200).json({
+            message: 'User added successfully!!',
+            data: savedUser,
+            accessToken: token,
+          });
+        } else {
+          return res.status(400).json({ message: 'User already exists' });
+        }
+      } catch (err) {
+        // console.log(err);
+        res.status(500).json({
+          message: 'There is a server side error',
+          // error: err
+        });
+      }
+    });
+
+    // console.log('DB connected!');
+  } finally {
+    // await client.close();
+  }
+};
+databaseConnect().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`server running at http://localhost:${port}`);
