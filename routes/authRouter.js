227383babef39581b5e3be752e2a81574a513a6a
muhammad-jiku <muhammadjiku364@gmail.com>
2023-01-29 17:53:24 +0600

    registration function added

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -47,6 +47,48 @@ const logIn = async (req, res) => {
   }
 };
 
+const Registration = async (req, res) => {
+  const { username, phone, email, password } = req.body;
+
+  try {
+    const oldUser = await User.findOne({ email: email });
+
+    if (!oldUser) {
+      const hashedPassword = await bcrypt.hash(password, 12);
+
+      const newUser = new User({
+        username: username,
+        phone: phone,
+        email: email,
+        password: hashedPassword,
+      });
+
+      const savedUser = await newUser.save();
+
+      const token = jwt.sign(
+        { email: savedUser.email },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: '86400s' }
+      );
+      console.log('creating token...', token);
+      res.status(200).json({
+        message: 'User added successfully!!',
+        data: savedUser,
+        accessToken: token,
+      });
+    } else {
+      return res.status(400).json({ message: 'User already exists' });
+    }
+  } catch (err) {
+    // console.log(err);
+    res.status(500).json({
+      message: 'There is a server side error',
+      // error: err
+    });
+  }
+};
+
 module.exports = {
   logIn,
+  Registration,
 };
