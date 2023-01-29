d4429d68f8f80cf93ee4348ba94f65dfcde38569
muhammad-jiku <muhammadjiku364@gmail.com>
2023-01-29 20:14:26 +0600

    redesigned

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const express = require('express');
 const cors = require('cors');
 
 const databaseConnect = require('./utils/dbConnect');
+const { logIn, registration } = require('./controllers/authController');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -20,12 +21,16 @@ app.use(cors(corsConfig));
 app.options('*', cors(corsConfig));
 app.use(express.json());
 
+//  database connection
+databaseConnect();
+
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'HELLO THERE!!' });
 });
 
-//  database connection
-databaseConnect();
+// setting-up application routes
+app.use('/api/login', logIn);
+app.use('/api/registration', registration);
 
 app.listen(port, () => {
   console.log(`server running at http://localhost:${port}`);
